7e28da58d60872e9c4f60ef0ed69075275032c34
ikemoti <ikepon413@icloud.com>
2020-01-27 18:33:30 +0900

    Initial Commit

---
+++ todotya/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todotya
+//
+//  Created by USER on 2020/01/27.
+//  Copyright © 2020 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
